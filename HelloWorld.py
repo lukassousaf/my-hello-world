3385264ae411c1267a558bd34d493f5ba42a0ca9
lukassousaf <56463436+lukassousaf@users.noreply.github.com>
2019-10-12 00:59:17 -0300

    Aprendendo a programar em python.

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+nome1 = input("digite seu nome: ")
+nome2 = input("digite seu nome: ")
+
+idade1 = int(input("digite a sua idade:"))
+idade2 = int(input("digite a sua idade:"))
+
+print("concatenado:", nome1,"e",nome2)
+print("somando:", idade1 + idade2)
